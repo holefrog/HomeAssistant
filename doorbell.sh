a92b340c179a00e0f015f27e8b345c7abf078362
holefrog <liu_dong@outlook.com>
2026-05-25 11:48:42 -0700

    Update doorbell script to fix VLC launch issue
    * Changed RTSP_URL to use Preview_01_main stream
    * Added FLAG_ACTIVITY_NEW_TASK to ensure VLC launches in a new full-screen task
    * Modified run_vlc function to include -f 0x10000000 flag for new task launch

--- doorbell.sh
+++ doorbell.sh
@@ -8,7 +8,7 @@ fi
 # 2. 定义常量配置
 DEVICE_X08A="192.168.50.180:5555"
 DEVICE_SONY_TV="192.168.50.220:5555"
-RTSP_URL="rtsp://admin:XH*8eSPx@192.168.50.190:554/Preview_01_sub"
+RTSP_URL="rtsp://admin:XH*8eSPx@192.168.50.190:554/Preview_01_main"
 VLC_ACTIVITY="org.videolan.vlc/org.videolan.vlc.gui.video.VideoPlayerActivity"
 INTENT_ACTION="android.intent.action.VIEW"
 KEYCODE_BACK=4
@@ -18,7 +18,8 @@ adb connect "$DEVICE_X08A"
 adb connect "$DEVICE_SONY_TV"
 
 run_vlc() {
-    adb -s "$1" shell cmd activity start -n "$VLC_ACTIVITY" -a "$INTENT_ACTION" -d "$RTSP_URL" --ez "from_start" true
+    # 添加了 -f 0x10000000 (FLAG_ACTIVITY_NEW_TASK) 确保以全新独立全屏任务启动
+    adb -s "$1" shell cmd activity start -f 0x10000000 -n "$VLC_ACTIVITY" -a "$INTENT_ACTION" -d "$RTSP_URL" --ez "from_start" true
 }
 
 close_vlc() {
