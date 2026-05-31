e1dd3ff36c3e9d2f997bd57ba4d766da2c407087
holefrog <liu_dong@outlook.com>
2026-05-30 19:17:07 -0700

    Update doorbell script to improve device handling
    * Changed keyevent in close_vlc function from KEYCODE_BACK to KEYCODE_HOME for better handling of VLC closure
    * Added KEYCODE_WAKEUP to run_vlc function to prevent screensaver and ensure device wakefulness
    * Introduced a 1-second delay after closing VLC to ensure animation completion before launching new activity
    * Modified activity start command to include FLAG_ACTIVITY_NEW_TASK for full-screen independent task launch

--- doorbell.sh
+++ doorbell.sh
@@ -11,19 +11,25 @@ DEVICE_SONY_TV="192.168.50.220:5555"
 RTSP_URL="rtsp://admin:XH*8eSPx@192.168.50.190:554/Preview_01_sub"
 VLC_ACTIVITY="org.videolan.vlc/org.videolan.vlc.gui.video.VideoPlayerActivity"
 INTENT_ACTION="android.intent.action.VIEW"
-KEYCODE_BACK=4
+KEYCODE_HOME=3
+KEYCODE_WAKEUP=224
 
 # 连接设备
 adb connect "$DEVICE_X08A"
 adb connect "$DEVICE_SONY_TV"
 
 run_vlc() {
+    # 先调用 close 退回桌面，并等待1秒确保动画执行完毕
+    close_vlc "$1"
+    sleep 1
+    # 唤醒屏幕，强制打断 screensaver (屏保)
+    adb -s "$1" shell input keyevent "$KEYCODE_WAKEUP"
     # 添加了 -f 0x10000000 (FLAG_ACTIVITY_NEW_TASK) 确保以全新独立全屏任务启动
     adb -s "$1" shell cmd activity start -f 0x10000000 -n "$VLC_ACTIVITY" -a "$INTENT_ACTION" -d "$RTSP_URL" --ez "from_start" true
 }
 
 close_vlc() {
-    adb -s "$1" shell input keyevent "$KEYCODE_BACK"
+    adb -s "$1" shell input keyevent "$KEYCODE_HOME"
 }
 
 # 3. 根据传入的参数执行对应的操作
