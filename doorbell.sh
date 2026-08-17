cb0a6440ebbae78bc5896af0c46fea5472e69772
holefrog <liu_dong@outlook.com>
2026-08-16 19:57:31 -0700

    chore: update doorbell.sh SMB configuration credentials and pathing

--- doorbell.sh
+++ doorbell.sh
@@ -9,19 +9,22 @@ fi
 DEVICE_REOLINK="192.168.50.86:554"
 DEVICE_X08A="192.168.50.180:5555"
 DEVICE_SONY_TV="192.168.50.220:5555"
+DEVICE_LENOVO_IDEA_TAB="192.168.50.160"
 RTSP_URL="rtsp://admin:XH*8eSPx@${DEVICE_REOLINK}/Preview_01_sub"
+RTSP_URL_MAIN="rtsp://admin:XH*8eSPx@${DEVICE_REOLINK}/Preview_01_main"
 VLC_ACTIVITY="org.videolan.vlc/org.videolan.vlc.gui.video.VideoPlayerActivity"
 INTENT_ACTION="android.intent.action.VIEW"
 KEYCODE_HOME=3
 KEYCODE_WAKEUP=224
 
 run_vlc() {
-    # 每次按需连接（静默执行，即便断线也能快速重连）
+    # $1: 设备地址  $2: RTSP URL（可选，默认用 RTSP_URL）
+    URL="${2:-$RTSP_URL}"
     adb connect "$1" >/dev/null 2>&1
-    adb -s "$1" shell input keyevent "$KEYCODE_HOME"
-    sleep 0.2
-    # 将多次 adb 通信合并为一次执行，降低网络延迟开销
-    adb -s "$1" shell "input keyevent $KEYCODE_WAKEUP && cmd activity start -f 0x10000000 -n '$VLC_ACTIVITY' -a '$INTENT_ACTION' -d '$RTSP_URL' --ez 'from_start' true"
+    # 先唤屏，再等摄像头处理完门铃事件（录制启动、推送通知等）后再建立 RTSP 连接
+    adb -s "$1" shell "input keyevent $KEYCODE_HOME && input keyevent $KEYCODE_WAKEUP"
+    sleep 3
+    adb -s "$1" shell "cmd activity start -f 0x10000000 -n '$VLC_ACTIVITY' -a '$INTENT_ACTION' -d '$URL' --ez 'from_start' true"
 }
 
 close_vlc() {
@@ -34,9 +37,11 @@ if [ "$1" = "show" ]; then
     # 使用 & 放入后台并发执行两台设备，最后 wait 同步退出
     run_vlc "$DEVICE_X08A" &
     run_vlc "$DEVICE_SONY_TV" &
+    run_vlc "$DEVICE_LENOVO_IDEA_TAB" "$RTSP_URL_MAIN" &  # Lenovo 用主码流，避免 sub 流并发超限
     wait
 elif [ "$1" = "close" ]; then
     close_vlc "$DEVICE_X08A" &
     close_vlc "$DEVICE_SONY_TV" &
+    close_vlc "$DEVICE_LENOVO_IDEA_TAB" &
     wait
 fi
